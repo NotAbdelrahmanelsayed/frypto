4263f3dc0d9074708dc124d8f570fd66d3f2cc6a
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2024-10-22 13:36:15 +0300

    testing multiple environments

--- setup.py
+++ setup.py
@@ -1,23 +1,2 @@
-from setuptools import find_packages, setup
-import pathlib
-
-curr_path = pathlib.Path(__file__).parent.resolve()
-
-# Get long description from readme file.
-long_description = (curr_path/"README.md").read_text(encoding="utf-8")
-
-setup(
-    name = "frypto",
-    version = "0.1.0",
-    description = "Crypto data feature engineering package.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Abderlahman Elsayed",
-    author_email="bedoelsayed785@gmail.com",
-    packages=find_packages(where="."),
-    install_requires=[  
-        "pandas",
-        "numpy",
-        "scipy",
-        "numba"],
-)
+from setuptools import setup
+setup()
